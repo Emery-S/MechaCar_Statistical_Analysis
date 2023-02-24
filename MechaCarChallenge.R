bb8350e3a072f1134dc2e6c408fd9d46487e0047
Emery-S <112206035+Emery-S@users.noreply.github.com>
2023-02-24 04:59:05 -0500

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,11 +18,29 @@ SusCoil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=
 head(SusCoil)
 
 # create a data frame for the suspension coil PSI summary
-total_summary <- SusCoil %>% summarize(Mean=mean(PSI),Median=median(PSI), Varience=var(PSI),Std_Dev =sd(PSI), Num_Coil=n() ,.groups = 'keep')
-
-
-
+total_summary <- SusCoil %>% summarize(Mean=mean(PSI),
+                                       Median=median(PSI),
+                                       Varience=var(PSI),
+                                       Std_Dev =sd(PSI),
+                                       Num_Coil=n(),
+                                       .groups = 'keep')
 
 # create a data frame for the suspension coil lot summary 
-lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Varience=var(PSI),Std_Dev =sd(PSI), Num_Coil=n() ,.groups = 'keep')
+lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI),
+            Median=median(PSI),
+            Varience=var(PSI),
+            Std_Dev =sd(PSI), 
+            Num_Coil=n() ,
+            .groups = 'keep')
+
+?t.test()
+
+# Use t-test to determine PSI across all Manufacturing_lots
+t.test(SusCoil$PSI, mu=1500)
+
+# Use t-test and subset by lot
+t.test(subset(SusCoil, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(SusCoil, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(SusCoil, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
 
